43e1311460e162f0c729da58c6b2ce4c1bc066aa
Lee SeungA <dltmddk981119@gmail.com>
2024-08-03 21:16:32 +0900

    20240803333 inflearn java-start end

--- src/method/MethodValue1.java
+++ src/method/MethodValue1.java
@@ -2,7 +2,7 @@ package method;
 
 public class MethodValue1 {
     public static void main(String[] args) {
-        int num1 = 51;
+        int num1 = 5;
         System.out.println("1. changeNumber 호출 전, num1: " + num1);
         changeNumber(num1);
         System.out.println("4. changeNumber 변경 후, num1 : " + num1);
